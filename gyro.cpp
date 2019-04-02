7d4c44685553871614d4e0845c40b24f06acb989
Suirevax <richard.janssen@student.hu.nl>
2019-04-02 16:08:09 +0200

    Update gyro.cpp

--- gyro.cpp
+++ gyro.cpp
@@ -78,26 +78,6 @@ int main(){
 			lspeed = normal_speed;
 			rspeed = normal_speed;
 		}
-
-    // printf("Gyro sensor: degrees per second %4d  \n", Gyro4.abs);
-		//
-    // BP.set_motor_power(PORT_C, 25);
-    // BP.set_motor_power(PORT_B, 25);
-		// BP.get_sensor(PORT_4, &Gyro4);
-		//
-		// printf("Gyro sensor: degrees per second %4d  \n", Gyro4.abs);
-		// sleep(3);
-		// BP.set_motor_power(PORT_C, 0);
-		// BP.set_motor_power(PORT_B, 0);
-		// sleep(1);
-		// BP.set_motor_power(PORT_C, -25);
-		// BP.set_motor_power(PORT_B, -25);
-		// BP.get_sensor(PORT_4, &Gyro4);
-		//
-		// printf("Gyro sensor: degrees per second %4d  \n", Gyro4.abs);
-		// sleep(3);
-		// BP.set_motor_power(PORT_C, 0);
-		// BP.set_motor_power(PORT_B, 0);
 		usleep(250000);
   }
 }
