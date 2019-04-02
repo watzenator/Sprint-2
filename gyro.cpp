3b1405d46fe869de90f1adc4f1b8ae8c9b96d7c5
Suirevax <richard.janssen@student.hu.nl>
2019-04-02 13:49:26 +0200

    Update gyro.cpp

--- gyro.cpp
+++ gyro.cpp
@@ -10,24 +10,24 @@
 
 
 // Signal handler that will be called when Ctrl+C is pressed to stop the program
-void exit_signal_handler(int signo){
-if(signo == SIGINT){
-	BP.reset_all();    // Reset everything so there are no run-away motors
-	exit(-2);
-}
-}
-//Checks the Batttery.
-bool voltageIsSafe(){
-	printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
-  	printf("9v voltage      : %.3f\n", BP.get_voltage_9v());
-  	printf("5v voltage      : %.3f\n", BP.get_voltage_5v());
-  	printf("3.3v voltage    : %.3f\n", BP.get_voltage_3v3());
-
-	if(BP.get_voltage_battery() < 10.9){
-		return false;
-	}
-	return true;
-}
+// void exit_signal_handler(int signo){
+// if(signo == SIGINT){
+// 	BP.reset_all();    // Reset everything so there are no run-away motors
+// 	exit(-2);
+// }
+// }
+// //Checks the Batttery.
+// bool voltageIsSafe(){
+// 	printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
+//   	printf("9v voltage      : %.3f\n", BP.get_voltage_9v());
+//   	printf("5v voltage      : %.3f\n", BP.get_voltage_5v());
+//   	printf("3.3v voltage    : %.3f\n", BP.get_voltage_3v3());
+//
+// 	if(BP.get_voltage_battery() < 10.9){
+// 		return false;
+// 	}
+// 	return true;
+// }
 
 int main(){
   signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
