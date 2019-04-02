fd781a86b65175f385ffede815e76e7ad7adfbc7
Suirevax <richard.janssen@student.hu.nl>
2019-04-02 14:16:29 +0200

    Update gyro.cpp

--- gyro.cpp
+++ gyro.cpp
@@ -49,8 +49,8 @@ int main(){
   while(true){
 		BP.set_motor_power(PORT_C, 25);
 		BP.set_motor_power(PORT_B, 25);
+		BP.get_sensor(PORT_4, &Gyro4);
 		while(true){
-			BP.get_sensor(PORT_4, &Gyro4);
 			printf("Gyro sensor: degrees per second %4d  \n", Gyro4.abs);
 		}
 
