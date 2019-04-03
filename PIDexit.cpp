8c36aa15cce5802ee636f1cf00e8b91fe31e6534
Watzez <43240975+Watzez@users.noreply.github.com>
2019-04-03 15:38:17 +0200

    Update PIDexit.cpp

--- PIDexit.cpp
+++ PIDexit.cpp
@@ -105,7 +105,31 @@ void goleft(){
       		usleep(1);
 	}
 }
-	
+
+void turnaround(){
+	sensor_gyro_t Gyro4;
+	BP.get_sensor(PORT_4, &Gyro4);
+	usleep(1);
+	int gyro_offset = Gyro4.abs;
+	bool slow_down_started;
+    	BP.set_motor_power(PORT_C, -100);
+    	BP.set_motor_power(PORT_B, 100);
+	slow_down_started = false;
+	while(true){
+      		BP.get_sensor(PORT_4, &Gyro4);
+      		std::cout << Gyro4.abs%360 << "        " << gyro_offset << '\n';
+      		if(Gyro4.abs - gyro_offset <= -160 || slow_down_started){
+        		slow_down_started = true;
+        		BP.set_motor_power(PORT_C, -20);
+        		BP.set_motor_power(PORT_B, 20);
+        		if(Gyro4.abs - gyro_offset <= -180){
+          			break;
+        		}
+      		}
+      		usleep(1);
+	}
+}
+
 void intersection(bool& sensorLeft, bool& sensorRight){
 	BP.set_motor_power(PORT_C, 0);
 	BP.set_motor_power(PORT_B, 0);
@@ -240,7 +264,10 @@ void exit_signal_handler(int signo){
 			baseline += 90;
 		}else if(input == "a"){
 			goleft();
-			baseline += -90;
+			baseline -= 90;
+		}else if(input == "s"){
+			turnaround();
+			baseline -= 180;
 		}
 	}
 }
