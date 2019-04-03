a04752a1a3e5ab1c9c53fed4a0cd2b56b325d038
Watzez <43240975+Watzez@users.noreply.github.com>
2019-04-03 15:07:37 +0200

    Update PIDexit.cpp

--- PIDexit.cpp
+++ PIDexit.cpp
@@ -32,9 +32,9 @@ void PIDconfig(pid & Pid){
 
 int PIDcontrol(pid & Pid, int setting, sensor_gyro_t & Gyro4){
 	int error = 0;
-	if(Gyro4.abs > setting && Gyro4.abs < setting + 5){
+	if(Gyro4.abs > setting && Gyro4.abs < setting - 5){
 		error = 5;
-	}else if(Gyro4.abs < setting && Gyro4.abs > setting - 5){
+	}else if(Gyro4.abs < setting && Gyro4.abs > setting + 5){
 		error = -5;
 	}else{ error = setting + Gyro4.abs % 360;
 	}
