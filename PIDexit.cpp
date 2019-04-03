7ab433661705c47a74ce8cc875a247734c4cea44
Watzez <43240975+Watzez@users.noreply.github.com>
2019-04-03 16:34:48 +0200

    Update PIDexit.cpp

--- PIDexit.cpp
+++ PIDexit.cpp
@@ -3,16 +3,14 @@
 #include <unistd.h>     // for usleep
 #include <signal.h>     // for catching exit signals
 #include <iostream>
-
+using namespace std;
 
 #define MAX_MOTORPOWER 100
 #define MIN_MOTORPOWER -100
 #define MOTORSPEED 90
 
 int baseline = 0;
-
-using namespace std;
-
+void exit_signal_handler(int signo);
 BrickPi3 BP;
 
 struct pid{
@@ -55,8 +53,6 @@ bool voltageIsSafe(){
 	}
 	return true;
 }
-
-void exit_signal_handler(int signo);
 
 void goright(){
 	sensor_gyro_t Gyro4;
@@ -130,33 +126,6 @@ void turnaround(){
 	}
 }
 
-void intersection(bool& sensorLeft, bool& sensorRight){
-	BP.set_motor_power(PORT_C, 0);
-	BP.set_motor_power(PORT_B, 0);
-	printf("Welke kant wil je op: \n");
-	int choice = getchar();
-
-	if(choice == '0'){
-		BP.set_motor_power(PORT_C, 15);
-		BP.set_motor_power(PORT_B, 15);
-		sleep(1);
-		BP.set_motor_power(PORT_C, -MOTORSPEED-10);
-		BP.set_motor_power(PORT_B, MOTORSPEED+10);
-		sleep(1);
-	}else if(choice == '1'){
-		BP.set_motor_power(PORT_C, 15);
-		BP.set_motor_power(PORT_B, 15);
-		sleep(1);
-		BP.set_motor_power(PORT_B, -MOTORSPEED-10);
-		BP.set_motor_power(PORT_C, MOTORSPEED+10);
-		sleep(1);
-	}else if(choice == '2'){
-		BP.set_motor_power(PORT_C, MOTORSPEED);
-		BP.set_motor_power(PORT_B, MOTORSPEED);
-		sleep(1);
-	}
-}
-
 void objects(int getal){
 		BP.set_motor_power(PORT_B, ((getal - 5) * 2));
 		BP.set_motor_power(PORT_C, ((getal - 5) * 2));
@@ -188,8 +157,7 @@ void objects(int getal){
 
 int main(){
 	signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
- 
-	BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
+ 	BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
 	
 	if(!voltageIsSafe){
 		printf("Battery almost empty, exiting program...");
@@ -206,7 +174,6 @@ int main(){
 	BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_LIGHT_ON);
 	BP.set_sensor_type(PORT_4, SENSOR_TYPE_EV3_GYRO_ABS_DPS);
 
-
 	sensor_light_t Light1;
 	sensor_ultrasonic_t Ultrasonic2;
 	sensor_light_t Light3;
@@ -214,16 +181,6 @@ int main(){
 
 	int8_t speedLeft = MOTORSPEED;
 	int8_t speedRight = MOTORSPEED;
-
-	bool sensorLeft = false;
-	bool sensorRight = false;
-	bool sensorTouch = false;
-
-	if(!voltageIsSafe){
-		printf("Battery almost empty, exiting program...");
-		BP.reset_all();
-		exit(-5);
-	}
 	
 	sleep(2);	
 	pid Pid;
@@ -239,12 +196,12 @@ int main(){
 		BP.get_sensor(PORT_2, &Ultrasonic2);
 		BP.get_sensor(PORT_3, &Light3);
 		BP.get_sensor(PORT_4, &Gyro4);
+		
 		int controlValue = PIDcontrol(Pid, baseline, Gyro4);
 		BP.set_motor_power(PORT_C, -controlValue + MOTORSPEED);
 		BP.set_motor_power(PORT_B, +controlValue + MOTORSPEED);
 		printf("Gyro abs: %4d \n", Gyro4.abs);
 		usleep(1);
-	
 	}
 }
 
