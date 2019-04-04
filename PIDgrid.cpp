d2628f8d2d9d20066d0132059849e5b22fc7abfb
Richard Janssen <rxpjanssen@hotmail.com>
2019-04-04 20:44:41 +0200

    fixed shit?

--- PIDgrid.cpp
+++ PIDgrid.cpp
@@ -24,13 +24,13 @@ int main(){
 	signal(SIGINT, exit_signal_handler); // register the exit function for ctrl+c
  	BP.detect(); // make sure that the brickpi3 is communicating and that the firmware is compatible with the drivers.
 
-	if(!voltageissafe){
+	if(!voltageIsSafe){
 		std::cout << "battery almost empty, exiting program...";
-		bp.reset_all();
+		BP.reset_all();
 		exit(-5);
 	}
 
-	BP.set_sensor_type(PORT_4, sensor_type_ev3_gyro_abs);
+	BP.set_sensor_type(PORT_4, SENSOR_TYPE_EV3_GYRO_ABS);
 
 	sensor_gyro_t gyro4;
 
@@ -38,12 +38,12 @@ int main(){
 	int8_t speedright = MOTORSPEED;
 
 
-	while(bp.get_sensor(port_4, &gyro4)){}
+	while(BP.get_sensor(PORT_4, &gyro4)){}
 	while(true){
 		std::cout << "vul startlocatie in\n";
-		location startlocation = asklocation();
+		location startlocation = askLocation();
 		std::cout << "vul eindlocatie in\n";
-		location endlocation = asklocation();
+		location endlocation = askLocation();
 		grid(startlocation, endlocation, gyro4);
 	}
 }
