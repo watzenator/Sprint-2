e0028191b8effae24cfdd98eb0bff5ec53511c3d
Suirevax <richard.janssen@student.hu.nl>
2019-04-05 14:44:56 +0200

    poging wieweet

--- PIDgrid.cpp
+++ PIDgrid.cpp
@@ -18,6 +18,44 @@ BrickPi3 BP;
 int baseline = 0;
 
 
+
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+	if(signo == SIGINT){
+		BP.set_motor_power(PORT_C, 0);
+		BP.set_motor_power(PORT_B, 0);
+		std::string input;
+		std::cout << "Give me a choice: ";
+		getline(std::cin, input);
+		if(input == "q"){
+			BP.reset_all();    // Reset everything so there are no run-away motors
+			exit(-2);
+		}else if(input == "d"){
+			goright();
+		}else if(input == "a"){
+			goleft();
+		}else if(input == "s"){
+			turnaround();
+		}
+	}
+}
+
+bool voltageIsSafe(){
+	  // printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
+  	// printf("9v voltage      : %.3f\n", BP.get_voltage_9v());
+  	// printf("5v voltage      : %.3f\n", BP.get_voltage_5v());
+  	// printf("3.3v voltage    : %.3f\n", BP.get_voltage_3v3());
+
+	if(BP.get_voltage_battery() < 10.9){
+		return false;
+	}
+	return true;
+}
+
+
+
+
+
 void goright(){
 	sensor_gyro_t Gyro4;
 	BP.get_sensor(PORT_4, &Gyro4);
@@ -250,7 +288,7 @@ void grid(location startLoc, location endLoc,sensor_gyro_t & Gyro4){
 		BP.set_motor_power(PORT_B, 0);
 	}
 }
-#endif
+
 
 
 
