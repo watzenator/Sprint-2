44b5222f4f0075c9679d6e33aaffc664f7a40eff
Suirevax <richard.janssen@student.hu.nl>
2019-04-05 15:41:09 +0200

    voltage info

--- signalhandler.h
+++ signalhandler.h
@@ -17,10 +17,10 @@ void exit_signal_handler(int signo){
 }
 
 bool voltageIsSafe(){
-	  // printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
-  	// printf("9v voltage      : %.3f\n", BP.get_voltage_9v());
-  	// printf("5v voltage      : %.3f\n", BP.get_voltage_5v());
-  	// printf("3.3v voltage    : %.3f\n", BP.get_voltage_3v3());
+	  printf("Battery voltage : %.3f\n", BP.get_voltage_battery());
+  	printf("9v voltage      : %.3f\n", BP.get_voltage_9v());
+  	printf("5v voltage      : %.3f\n", BP.get_voltage_5v());
+  	printf("3.3v voltage    : %.3f\n", BP.get_voltage_3v3());
 
 	if(BP.get_voltage_battery() < 10.9){
 		return false;
