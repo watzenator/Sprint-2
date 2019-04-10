693a2c8638f134f69b0cf2773106f223cc6e59c3
Richard Janssen <rxpjanssen@hotmail.com>
2019-04-10 19:26:23 +0200

    system()

--- soundtest.cpp
+++ soundtest.cpp
@@ -4,8 +4,10 @@
 
 int main(){
   signal(SIGCHLD, SIG_IGN);
-  std::cout << "start\n";
-  left_sound();
+  // std::cout << "start\n";
+  // left_sound();
+  // std::cout << "end\n";
+  system("aplay left.wav");
   std::cout << "end\n";
   return 0;
 }
