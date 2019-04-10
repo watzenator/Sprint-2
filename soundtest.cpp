20cceb54c8dc2e86b4b74ab20861ba6e5bb750e4
Richard Janssen <rxpjanssen@hotmail.com>
2019-04-10 19:33:41 +0200

    quiet2

--- soundtest.cpp
+++ soundtest.cpp
@@ -4,10 +4,8 @@
 
 int main(){
   signal(SIGCHLD, SIG_IGN);
-  // std::cout << "start\n";
-  // left_sound();
-  // std::cout << "end\n";
-  system("aplay left.wav");
+  std::cout << "start\n";
+  left_sound();
   std::cout << "end\n";
   return 0;
 }
