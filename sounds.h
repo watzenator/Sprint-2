c3f48289d66bf4819622491e49788f64e01ea8c7
Richard Janssen <rxpjanssen@hotmail.com>
2019-04-10 18:43:40 +0200

    fixed typo's

--- sounds.h
+++ sounds.h
@@ -3,31 +3,31 @@
 #include <unistd.h>
 
 
-void kill_all_sound(){
-
-}
-
-void forward_sound(){
-  if(fork() = 0){
-    //child
-    char* args[] = {(char*)"/usr/bin/aplay",(char*) "forward.wav", NULL}
-    execv(args[0], args);
-    kill(getpid(), SIGKILL);
-  }else{
-    //parent
-    return
-  }
-}
+// void kill_all_sound(){
+//
+// }
+//
+// void forward_sound(){
+//   if(fork() = 0){
+//     //child
+//     char* args[] = {(char*)"/usr/bin/aplay",(char*) "forward.wav", NULL}
+//     execv(args[0], args);
+//     kill(getpid(), SIGKILL);
+//   }else{
+//     //parent
+//     return
+//   }
+// }
 
 void left_sound(){
-  if(fork() = 0){
+  if(fork() == 0){
     //child
     char* args[] = {(char*)"/usr/bin/aplay",(char*) "./left.wav", NULL}
     execv(args[0], args);
     //kill(getpid(), SIGKILL);
-    exit();
+    exit(0);
   }else{
     //parent
-    return
+    return;
   }
 }
