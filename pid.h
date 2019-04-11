f4997a02e165939b6fe5b3e8027e7a27a5154d0f
Suirevax <richard.janssen@student.hu.nl>
2019-04-11 15:25:32 +0200

    upped gain

--- pid.h
+++ pid.h
@@ -20,7 +20,7 @@ int calc_degrees(int abs){
 int PIDcontrol(sensor_gyro_t & Gyro4){
 	BP.get_sensor(PORT_4, &Gyro4);
 	//std::cout << Gyro4.abs << '\n';
-	int turnspeed = calc_degrees(Gyro4.abs - baseline) * 0.8;
+	int turnspeed = calc_degrees(Gyro4.abs - baseline) * 1;
 	if(turnspeed > MAX_MOTORPOWER) return MAX_MOTORPOWER;
 	if(turnspeed > -30 && turnspeed < 0) return -30;
 	if(turnspeed < 30 && turnspeed > 0) return 30;
