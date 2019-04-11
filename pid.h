6a2ca97df025b13fd0d5f2348e528a897e2f05e8
Suirevax <richard.janssen@student.hu.nl>
2019-04-11 15:23:18 +0200

    commented cout

--- pid.h
+++ pid.h
@@ -19,7 +19,7 @@ int calc_degrees(int abs){
 
 int PIDcontrol(sensor_gyro_t & Gyro4){
 	BP.get_sensor(PORT_4, &Gyro4);
-	std::cout << Gyro4.abs << '\n';
+	//std::cout << Gyro4.abs << '\n';
 	int turnspeed = calc_degrees(Gyro4.abs - baseline) * 0.8;
 	if(turnspeed > MAX_MOTORPOWER) return MAX_MOTORPOWER;
 	if(turnspeed > -30 && turnspeed < 0) return -30;
